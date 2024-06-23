dfeb00b3711d2fb89dd2dc39be168f3d545f6b45
onezeun <onezeun@gmail.com>
2024-06-23 14:54:42 +0900

    feat: 프로젝트 세팅, 라우터 설정

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import Details from "pages/Details";
+
+import Layout from "components/Layout";
+
+const App = () => {
+  let element = useRoutes([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          path: "/character/:id",
+          element: <Details />,
+        },
+      ],
+    },
+  ]);
+  return element;
+}
+
+export default App;
